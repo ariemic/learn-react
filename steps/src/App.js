85417231cf01130f877e02515b3774b3ce106664
Ariel <arie.michalik@gmail.com>
2024-08-26 13:15:11 +0200

    steps new

--- steps/src/App.js
+++ steps/src/App.js
@@ -31,9 +31,8 @@ export default function App() {
             <div className={`${step >= 2 ? "active" : ""}`}>2</div>
             <div className={`${step >= 3 ? "active" : ""}`}>3</div>
           </div>
-          <p className="message">
-            Step {step}: {messages[step - 1]}
-          </p>
+
+          <StepMessage step={step}>{messages[step - 1]}</StepMessage>
 
           <div className="buttons">
             <Button
@@ -64,5 +63,14 @@ function Button({ onClickFun, bgColor, textColor, children }) {
     >
       {children}
     </button>
+  );
+}
+
+function StepMessage({ step, children }) {
+  return (
+    <div className="message">
+      <h3>Step {step}:</h3>
+      {children}
+    </div>
   );
 }

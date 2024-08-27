13ac2aa8e6a779913abcd0aa5087477ab9000ba5
Ariel <arie.michalik@gmail.com>
2024-08-27 12:21:37 +0200

    eat n split static version

--- eat-n-split/src/App.js
+++ eat-n-split/src/App.js
@@ -1,3 +1,108 @@
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
+
 export default function App() {
-  return <div>siema</div>;
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <FriendsList></FriendsList>
+        <FormAddFriend></FormAddFriend>
+        <Button>Add friend</Button>
+      </div>
+      <FormSplitBill></FormSplitBill>
+    </div>
+  );
+}
+
+function FriendsList() {
+  const friends = initialFriends;
+
+  return (
+    <ul>
+      {friends.map((el) => (
+        <Friend friend={el} />
+      ))}
+    </ul>
+  );
+}
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt="profile" />
+      <h3>{friend.name}</h3>
+
+      {friend.balance < 0 ? (
+        <p className="red">
+          You owe {friend.name} {-friend.balance}$
+        </p>
+      ) : friend.balance > 0 ? (
+        <p className="green">
+          {friend.name} owes you {friend.balance}$
+        </p>
+      ) : (
+        <p>You and {friend.name} are even</p>
+      )}
+
+      <Button>Select</Button>
+    </li>
+  );
+}
+
+function Button({ children }) {
+  return <div className="button">{children}</div>;
+}
+
+function FormAddFriend() {
+  return (
+    <form className="form-add-friend">
+      <label>🧑‍🤝‍🧑 Friend name</label>
+      <input type="text" />
+
+      <label>🖼️ Image URL</label>
+      <input type="text" />
+
+      <Button>Add</Button>
+    </form>
+  );
+}
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>split a bill with </h2>
+      <label>💰 Bill value</label>
+      <input type="text" />
+
+      <label>🧍 Your expense</label>
+      <input type="text" />
+
+      <label>🧑‍🤝‍🧑 X's expense:</label>
+      <input type="text" disabled />
+
+      <label>🤑 Who is paying the bill?</label>
+      <select>
+        <option value="user">You</option>
+        <option value="friend">X</option>
+      </select>
+
+      <Button>Split bill</Button>
+    </form>
+  );
 }
